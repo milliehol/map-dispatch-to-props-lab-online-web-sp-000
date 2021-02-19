e9ab10588e499704691fa7ae84241430882487ac
Carla Holcomb <carla.programmer@gmail.com>
2021-02-19 04:57:49 +0000

    Automatically backed up by Learn

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -60,4 +60,4 @@ const mapDispatchToProps = dispatch => {
 
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(null, mapDispatchToProps)(RestaurantInput);
